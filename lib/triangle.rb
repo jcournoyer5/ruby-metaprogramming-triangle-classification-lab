04184663b62fee111709f31e75c9ded0bcb60ee6
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2021-01-23 19:04:56 +0000

    commit tree 989151719205c13a8e5db06e3face6f125beef72

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
-end
+  
+attr_reader :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+  
+  
+  def kind
+     validate_triangle
+    if side_a == side_b && side_b == side_c
+      :equilateral
+    elsif side_a == side_b || side_b == side_c || side_a == side_c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real_triangle = [(side_a + side_b > side_c), (side_a + side_c > side_b), (side_b + side_c > side_a)]
+    [side_a, side_b, side_c].each do |side|
+      real_triangle << false if side <= 0 
+    raise TriangleError if real_triangle.include?(false)
+    end
+  end
+  
+    class TriangleError < StandardError
+    # triangle error code
+    end
+end 
+  
+
